a17f0ae022f0015e21969f0e1d6266ec45b826c6
xZaeckk <iamzaeck@gmail.com>
2026-07-25 16:54:05 +1000

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -17,3 +17,40 @@ window.onload = function () {
     }
 
 }
+const submitBtn = document.getElementById("submitPurchase");
+
+if (submitBtn) {
+
+    submitBtn.addEventListener("click", function () {
+
+        // Hide checkout form
+        document.getElementById("checkoutForm").style.display = "none";
+
+        // Show confirmation page
+        document.getElementById("confirmPage").style.display = "block";
+
+        // Copy information
+        document.getElementById("confirmUsername").textContent =
+            document.getElementById("minecraftName").value;
+
+        document.getElementById("confirmEmail").textContent =
+            document.getElementById("email").value;
+
+        document.getElementById("confirmPlatform").textContent =
+            document.getElementById("platform").value;
+
+        document.getElementById("confirmGamemode").textContent =
+            document.getElementById("gamemode").value;
+
+        document.getElementById("confirmRank").textContent =
+            document.getElementById("rank").value;
+
+        document.getElementById("confirmPayment").textContent =
+            document.querySelector("input[name='payment']:checked").value;
+
+        document.getElementById("confirmPrice").textContent =
+            document.getElementById("price").value;
+
+    });
+
+}
