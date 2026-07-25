9f3d7e49b9ed3905b62871cad61fa61a04cf1b6d
xZaeckk <iamzaeck@gmail.com>
2026-07-25 16:50:30 +1000

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,1 +1,19 @@
+window.onload = function () {
 
+    const rank = localStorage.getItem("rank");
+    const price = localStorage.getItem("price");
+
+    if (rank) {
+
+        const rankSelect = document.getElementById("rank");
+        if (rankSelect) rankSelect.value = rank;
+
+        const product = document.getElementById("product");
+        if (product) product.value = rank + " Rank";
+
+        const priceBox = document.getElementById("price");
+        if (priceBox) priceBox.value = "$" + Number(price).toFixed(2);
+
+    }
+
+}
